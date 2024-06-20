0cb91cdf2eefea0d4e4548e32547ad62f97a6134
Jaewon Noh <opqw987@gmail.com>
2024-06-20 17:12:57 +0900

    :bug: Application runner setter를 위한 createdAt val -> var 설정

--- src/main/kotlin/sparta/nbcamp/schedulercomment/domain/common/BaseTimeEntity.kt
+++ src/main/kotlin/sparta/nbcamp/schedulercomment/domain/common/BaseTimeEntity.kt
@@ -13,7 +13,7 @@ import java.time.LocalDateTime
 abstract class BaseTimeEntity {
     @CreatedDate
     @Column(updatable = false)
-    val createdAt: LocalDateTime = LocalDateTime.now()
+    var createdAt: LocalDateTime = LocalDateTime.now()
 
     @LastModifiedDate
     var updatedAt: LocalDateTime = LocalDateTime.now()
