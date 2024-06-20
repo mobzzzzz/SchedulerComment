a066f2d35dc70f1287b68f986b9d1ace3d7cedad
Jaewon Noh <opqw987@gmail.com>
2024-06-20 15:47:58 +0900

    :tada: Initialize Project

---
+++ src/main/kotlin/sparta/nbcamp/schedulercomment/SchedulerCommentApplication.kt
@@ -0,0 +1,11 @@
+package sparta.nbcamp.schedulercomment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchedulerCommentApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchedulerCommentApplication>(*args)
+}
---
+++ src/test/kotlin/sparta/nbcamp/schedulercomment/SchedulerCommentApplicationTests.kt
@@ -0,0 +1,12 @@
+package sparta.nbcamp.schedulercomment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchedulerCommentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
